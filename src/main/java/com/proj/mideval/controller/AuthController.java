82fb3839d25076114c640675be00ab06f406356b
Eshaan Sodhi <eshaansodhi@Eshaans-MacBook-Pro.local>
2024-11-03 20:48:27 +0530

    chemist auth

--- src/main/java/com/proj/mideval/controller/AuthController.java
+++ src/main/java/com/proj/mideval/controller/AuthController.java
@@ -1,9 +1,11 @@
 package com.proj.mideval.controller;
 
+import com.proj.mideval.model.Chemist;
 import com.proj.mideval.model.Patient;
 import com.proj.mideval.model.Doctor;
 import com.proj.mideval.model.Admin;
 import com.proj.mideval.service.AdminService;
+import com.proj.mideval.service.ChemistService;
 import com.proj.mideval.service.PatientService;
 import com.proj.mideval.service.DoctorService;
 import com.proj.mideval.util.JwtUtil;
@@ -29,6 +31,9 @@ public class AuthController {
     @Autowired
     private AdminService adminService;
 
+    @Autowired
+    private ChemistService chemistService;
+
     @GetMapping("/userinfo")
     public ResponseEntity<?> getUserInfo(@RequestHeader("Authorization") String token) {
         String email = JwtUtil.extractEmail(token);
@@ -46,6 +51,8 @@ public class AuthController {
                     userDetails.put("userType", "PATIENT");
                     userDetails.put("ntk", patient.getNTK());
                     userDetails.put("history", patient.getHistory());
+                } else {
+                    return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Patient not found");
                 }
                 break;
 
@@ -57,10 +64,35 @@ public class AuthController {
                     userDetails.put("lastName", doctor.getLastName());
                     userDetails.put("email", doctor.getEmail());
                     userDetails.put("userType", "DOCTOR");
+                    userDetails.put("specialization", doctor.getSpecialization());
+                    userDetails.put("department", doctor.getDepartment());
+                } else {
+                    return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Doctor not found");
                 }
                 break;
 
             case "ADMIN":
+                Optional<Admin> adminOpt = adminService.getAdminByEmail(email);
+                if (adminOpt.isPresent()) {
+                    Admin admin = adminOpt.get();
+                    userDetails.put("email", admin.getEmail());
+                    userDetails.put("userType", "ADMIN");
+                } else {
+                    return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Admin not found");
+                }
+                break;
+
+            case "CHEMIST":
+                Optional<Chemist> chemistOpt = chemistService.getChemistByEmail(email);
+                if (chemistOpt.isPresent()) {
+                    Chemist chemist = chemistOpt.get();
+                    userDetails.put("firstName", chemist.getFirstName());
+                    userDetails.put("lastName", chemist.getLastName());
+                    userDetails.put("email", chemist.getEmail());
+                    userDetails.put("userType", "CHEMIST");
+                } else {
+                    return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Chemist not found");
+                }
                 break;
 
             default:
@@ -93,9 +125,11 @@ public class AuthController {
             Optional<Doctor> doctor = doctorService.authenticateDoctor(email, password);
             if (doctor.isPresent()) {
                 token = JwtUtil.generateToken(email, "DOCTOR");
+                String id = doctorService.getDoctorIDByEmail(email).toString();
                 response.put("userType", "DOCTOR");
                 response.put("token", token);
                 response.put("email", email);
+                response.put("id", id);
                 return ResponseEntity.ok(response);
             } else {
                 return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid doctor credentials");
@@ -104,12 +138,27 @@ public class AuthController {
             Optional<Admin> adminOpt = adminService.authenticateAdmin(email, password);
             if (adminOpt.isPresent()) {
                 token = JwtUtil.generateToken(email, "ADMIN");
+                String id = adminService.getAdminIDByEmail(email).toString();
                 response.put("userType", "ADMIN");
                 response.put("token", token);
                 response.put("email", email);
+                response.put("id", id);
                 return ResponseEntity.ok(response);
             } else {
                 return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid admin credentials");
+            }
+        } else if ("CHEMIST".equalsIgnoreCase(userType)) {
+            Optional<Chemist> chemist = chemistService.authenticateChemist(email, password);
+            if (chemist.isPresent()) {
+                token = JwtUtil.generateToken(email, "CHEMIST");
+                String id = chemistService.getChemistIDByEmail(email).toString();
+                response.put("userType", "CHEMIST");
+                response.put("token", token);
+                response.put("email", email);
+                response.put("id", id);
+                return ResponseEntity.ok(response);
+            } else {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid chemist credentials");
             }
         }
         else {
